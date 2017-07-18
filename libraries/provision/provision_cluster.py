39bf86f484cd88a411857576bf201648ef92b391
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-07-18 10:06:24 -0700

    sync

--- libraries/provision/provision_cluster.py
+++ libraries/provision/provision_cluster.py
@@ -58,11 +58,13 @@ def provision_cluster(cluster_config, couchbase_server_config, sync_gateway_conf
     clean_cluster(cluster_config)
 
     # Install server package
-    log_info("Installing Couchbase Server")
+    log_info("Installing Couchbase Server - skipped")
+    '''
     install_couchbase_server.install_couchbase_server(
         cluster_config=cluster_config,
         couchbase_server_config=couchbase_server_config
     )
+    '''
 
     # Install sync_gateway
     log_info("Installing Sync Gateway")
